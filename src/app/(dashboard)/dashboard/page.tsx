0b5dd2230e6b68245c0d02469b65f994010712ff
John Ossai <johnossai20@gmail.com>
2024-12-31 10:51:51 +0100

    Implement role-based redirection in dashboard page

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,8 +1,16 @@
 import OverviewMain from "@/components/Dashboard/Pages/Overview/OverviewMain";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const DashboardIndexPage = () => {
-   return <OverviewMain />;
+const DashboardIndexPage = async () => {
+   const session = await getServerSession();
+
+   if (session?.user?.role == "STUDENT") {
+      return redirect("/dashboard/learning");
+   } else {
+      return <OverviewMain />;
+   }
 };
 
 export default DashboardIndexPage;
